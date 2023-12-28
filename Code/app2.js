fb3b867660670482efd2e12d8fbc0fd941aa51bf
The-IJ <112268637+The-IJ@users.noreply.github.com>
2023-12-28 15:14:28 +0530

    app2.js

---
+++ Code/app2.js
@@ -0,0 +1,16 @@
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCaeQiOlLDAl76NmWWFUlHbg&maxResults=10&order=date&key=")
+    .then((result) => {
+        return result.json()
+    }).then((data) => {
+        console.log(data)
+        let videos = data.items
+        // nextPageToken = data.nextPageToken
+        let videoContainer = document.querySelector(".youtube-container")
+        for (video of videos) {
+            videoContainer.innerHTML += `
+            <h3>${video.snippet.title}</h3>
+            <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank">
+            <img src="${video.snippet.thumbnails.default.url}">
+            `
+        }
+    })
